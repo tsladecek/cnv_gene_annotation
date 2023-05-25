45a070b94ac85a0de083c4eea65a48a0b9c7e32a
Tomas Sladecek <tomas.sladecek@geneton.sk>
2023-05-25 07:58:02 +0200

    read cnvs from input.bed file

--- scripts/input_function.py
+++ scripts/input_function.py
@@ -1,9 +1,26 @@
+import os
+
+
+cnv_types = { 'dup': 'gain', 'gain': 'gain', 'del': 'loss', 'loss': 'loss' }
+
 def all_cnvs(wildcards):
     """This should be populated with all CNVs desired to be annotated
     
     see example below with list of 2 CNVs
     """
-    res = []
-    
-    res = ['data/annotated_cnvs_json/1_500_1000000_gain.json.gz', 'data/annotated_cnvs_json/2_3000000_10000000_loss.json.gz']
-    return res
+    input_filename = 'input_example.bed' if 'input.bed' not in os.listdir() else 'input.bed'
+    with open(input_filename) as f:
+        
+        outputs = []
+        for cnv in f.readlines():
+            chrom, start, end, cnv_type = cnv.strip().split('\t')
+            chrom = chrom.strip('chr')
+            try:
+                cnv_type = cnv_types[cnv_type.lower()]
+            except KeyError:
+                exit(1)
+            else:
+                outputs.append(os.path.join('data', 'annotated_cnvs_json', f'{chrom}_{start}_{end}_{cnv_type}.json.gz'))
+
+    return outputs
+
